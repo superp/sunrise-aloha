f6b4668e6ac328da57e98f6b87235ef002d5238b
Igor Galeta <galeta.igor@gmail.com>
2012-01-18 18:46:37 +0200

    Save content by click

--- vendor/assets/javascripts/aloha_init.js
+++ vendor/assets/javascripts/aloha_init.js
@@ -1,29 +1,64 @@
 Aloha.ready(function(){
+  Aloha.checkContents = function(){
+    var elements = $('[modified]');
+    return elements.length == 0;
+  }
+  
+  Aloha.modifiedContents = function(){
+    var hash = {};
+    
+    $('[modified]').each(function(){
+      var url = $(this).data('url');
+      var name = $(this).attr('name');
+      
+      if (hash[url] === undefined || hash[url] === null) { 
+        hash[url] = {};
+      }
+      
+      hash[url][name] = $(this).html();
+    });
+    
+    return hash;
+  }
+  
+  Aloha.saveContents = function(){
+    var hash = Aloha.modifiedContents();
+    var options = {};
+
+    $.each(hash, function(key, value) { 
+      options = {
+        url: key, type: 'PUT', data: value, dataType: 'json',
+        success: function(data, status, xhr) {
+          // TODO
+        },
+        complete: function(xhr, status) {
+          // TODO
+        },
+        error: function(xhr, status, error) {
+          // TODO
+        }
+      };
+      
+      $.rails.ajax(options);
+    });
+  }
+    
   // save all changes after leaving an editable
   Aloha.require( ['aloha', 'aloha/jquery'], function( Aloha, jQuery) {
-    Aloha.bind('aloha-editable-deactivated', function(){
-      var content = Aloha.activeEditable.getContents();
-      var contentId = Aloha.activeEditable.obj[0].id;
-      var pageId = window.location.pathname;
-
-      // textarea handling -- html id is "xy" and will be "xy-aloha" for the aloha editable
-      if ( contentId.match(/-aloha$/gi) ) {
-        contentId = contentId.replace( /-aloha/gi, '' );
+    Aloha.bind('aloha-smart-content-changed', function(event, data) {
+      var editable = data.editable;
+      
+      if (editable.isModified()) {
+        editable.obj.attr('modified', event.timeStamp);
+        editable.setUnmodified();
+      } else {
+        return true;
       }
-
-      jQuery.ajax({
-        url: "save.php",
-        type: "POST",
-        data: {
-          content : content,
-          contentId : contentId,
-          pageId : pageId
-        },
-        dataType: "html"
-      });
     });
   });
  
   // Enable aloha on elements with contenteditable attribute
   Aloha.jQuery('[contenteditable]').aloha();
+  
+  $(window).bind('beforeunload', Aloha.checkContents);
 });
